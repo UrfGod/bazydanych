3d2e9976945b5d0d12fceef59bd46a8d9e2bd502
Mikołaj Palimąka <mpalimaka@zssalezjanie.com>
2017-05-24 12:09:02 +0200

    Dołączanie do bazy danych

--- index.php
+++ index.php
@@ -5,12 +5,17 @@ $dbhost = 'localhost';
 $dbuser = 'root';
 $dbpass = 'root';
 $dbname = 'artykuly';
-$connect = mysql_connect($dbhost, $dbuser,$dbpass);
+$connect = mysql_connect($dbhost,$dbuser,$dbpass);
 mysql_select_db($dbname,$connect);
 
-
-
-
-
-
 ?>
+
+<form action="GET">
+<input type="text" name="autor">
+<input type="text" name="kontent">
+<input type="sumbit" name="dodaj do bazy danych">
+</form>
+
+
+
+
