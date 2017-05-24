8176fa81ed5ed295a469648ee0a1685378925e60
Mikołaj Palimąka <mpalimaka@zssalezjanie.com>
2017-05-24 12:05:24 +0200

    Połączenie z bazą danych

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
 
 
+$dbhost = 'localhost';
+$dbuser = 'root';
+$dbpass = 'root';
+$dbname = 'artykuly';
+$connect = mysql_connect($dbhost, $dbuser,$dbpass);
+mysql_select_db($dbname,$connect);
 
 
 
